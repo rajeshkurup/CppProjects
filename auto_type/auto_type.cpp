343e4d4e4747a3b82718cd77fe6cd6fd513894f5
Rajesh Raghavakurup <rajeshraghavakurup@Rajeshs-Air.attlocal.net>
2022-12-12 22:03:25 -0800

    sample for auto types

--- auto_type/auto_type.cpp
+++ auto_type/auto_type.cpp
@@ -11,19 +11,21 @@ using py_dict = std::map<std::string, std::string>;
 using py_dict_map = std::map<std::string, py_dict>;
 
 int main() {
-    tweet << "============ Program to demonstrate auto data type ==============" << nline;
+    tweet << "============ Program to demonstrate auto data type ==============" << nline << nline;
 
     auto x = 123;
 
     tweet << "x=" << x << " - Type = " << typeid(x).name() << nline;
 
+    tweet << nline;
+
     int_set intSet = {34, 56, 23, 45, 67};
 
-    for(auto iter = intSet.begin(); iter != intSet.end(); iter++) {
-        tweet << *iter << " ";
+    for(const auto& val : intSet) {
+        tweet << val << " ";
     }
 
-    tweet << nline;
+    tweet << nline << nline;
 
     py_dict strMap = {{"key1", "val1"}, {"key2", "val2"}};
 
@@ -35,8 +37,8 @@ int main() {
 
     py_dict_map strMapOfMap = {{"super_key1", strMap}, {"super_key2", {{"key3", "val3"}, {"key4", "val4"}}}};
 
-    for(auto iter = strMapOfMap.begin(); iter != strMapOfMap.end(); iter++) {
-        for(auto iter2 = iter->second.begin(); iter2 != iter->second.end(); iter2++) {
+    for(const auto& tmpStrMapPair : strMapOfMap) {
+        for(auto iter2 = tmpStrMapPair.second.begin(); iter2 != tmpStrMapPair.second.end(); iter2++) {
             tweet << "key = " << iter2->first << ", value = " << iter2->second << nline;
         }
 
@@ -54,7 +56,7 @@ int main() {
     tweet << nline;
 
     if(strMapOfMap.contains("super_key1")) {
-        auto tmpStrMap = strMapOfMap.find("super_key1")->second;
+        auto& tmpStrMap = strMapOfMap.find("super_key1")->second;
 
         for(auto iter3 = tmpStrMap.begin(); iter3 != tmpStrMap.end(); iter3++) {
             tweet << "key = " << iter3->first << ", value = " << iter3->second << nline;
